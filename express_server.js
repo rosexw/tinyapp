60d87aeea2955c4ea26833f0526861e40bd9fc84
Rose Wan <rwan@mail.com>
2017-06-06 21:52:32 +0000

    EJS Adding a Second Route and Template

---
+++ express_server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Welcome!");
+});
+
+  app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+
+  app.get("/hello", (req, res) => {
+    res.end("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+  app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  app.get("/urls/:id", (req, res) => {
+    let templateVars = { shortURL: req.params.id };
+    res.render("urls_show", templateVars);
+  });
+  app.get("/urls/:id", (req, res) => {
+    let templateVars = { header: req.params.id };
+    res.render("_header", templateVars);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`);
+});
