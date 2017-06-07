8bb6b20906e256f1b888cb2b0ca11e6bf56bee39
Rose Wan <rwan@mail.com>
2017-06-07 17:00:24 +0000

    added protocol checker for links without https or http, changed express_server and urls_index to make the delete function working, and changed designs in header, Welcome page, and new. Format later this week.

--- express_server.js
+++ express_server.js
@@ -1,62 +1,84 @@
 var express = require("express");
 var app = express();
 var randomPass = require("./random.js");
-var PORT = process.env.PORT || 8080; // default port 8080
-
-app.set("view engine", "ejs");
+var protocolChecker = require("./protocolChecker.js");
+var PORT = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Welcome!");
-});
-
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-  app.get("/hello", (req, res) => {
-    res.end("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
-  app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
-  app.get("/urls/new", (req, res) => {
-    // console.log("/urls/new");
-    res.render("urls_new");
-  });
-  app.get("/urls/:id", (req, res) => {
-    let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
-  });
-  app.post("/urls", (req, res) => {
-    console.log(req.body);
-    var shortURL = randomPass();
-    res.redirect("/urls/" + shortURL);
-    // console.log(shortURL);
-  });
-  app.get("/urls/:id", (req, res) => {
-    let templateVars = { header: req.params.id };
-    res.render("_header", templateVars);
-  });
-
-  app.get("/u/:shortURL", (req, res) => {
-    let longURL = urlDatabase[req.params.shortURL];
-    if (longURL) {
-      res.redirect(longURL);
-    } else {
-      res.end("<html><body>URL not found</body></html>\n");
+//find URL
+function findURL(id) {
+  let foundURL;
+  urlDatabase.forEach((url) => {
+    if (url.id == id) {
+      foundURL = url;
     }
   });
+  return foundURL;
+};
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}!`);
+app.get("/", (req, res) => {
+  res.end("<html><body><h1>Welcome to TinyApp: a URL Shortener Tool!</h1></body></html>\n");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  // console.log("/urls/new");
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+//redirect shortURL to long URL
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.end("<html><body>URL not found</body></html>\n");
+  }
+});
+//creates random shortURL
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  var shortURL = randomPass();
+  res.redirect("/urls/" + shortURL);
+});
+
+//Delete URL
+app.post('/urls/:id/delete', (req, res) => {
+    // find url in database
+    const u = findURL(req.params.id);
+    // if does not exist return a 404
+    if (!u) {
+      res.status(404).send('URL not found.');
+      return;
+    }
+    // remove URL from database
+    const index = urlDatabase.indexOf(u);
+    urlDatabase.splice(index, 1);
+    // redirect to home
+    res.redirect('/urls');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
 });
---
+++ protocolChecker.js
@@ -0,0 +1,13 @@
+// We get an error if the protocol is not included in the link
+// Links without a valid protocol are modified to use HTTPS and returned
+
+function protocolChecker(url) {
+if (((url.substring(0,7)) === 'http://') || ((url.substring(0,8)) === 'https://')) {
+  return;
+} else {
+  let correctedURL = (`https://${url}`);
+  return correctedURL;
+  }
+};
+
+module.exports = protocolChecker;
